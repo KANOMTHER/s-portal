e07375d8bda7fe4f94600996a6a7a247da026221
Nontawat <mix_sk139@hotmail.com>
2024-04-11 18:25:53 +0000

    typo: change variable name

--- internal/domain/service/class_service.go
+++ internal/domain/service/class_service.go
@@ -18,32 +18,32 @@ func NewClassService(db *gorm.DB) *ClassService {
 	}
 }
 
-func (ps *ClassService) CreateClass(class *model.Class) error {
-	if err := ps.db.Create(&class).Error; err != nil {
+func (cs *ClassService) CreateClass(class *model.Class) error {
+	if err := cs.db.Create(&class).Error; err != nil {
 		return err
 	}
 	return nil
 }
 
-func (ps *ClassService) GetClassByID(id string) (*model.Class, error) {
+func (cs *ClassService) GetClassByID(id string) (*model.Class, error) {
 	var class *model.Class
-	if err := ps.db.Preload("Course").First(&class, id).Error; err != nil {
+	if err := cs.db.Preload("Course").First(&class, id).Error; err != nil {
 		return nil, err
 	}
 	return class, nil
 }
 
-func (ps *ClassService) GetClassByCourseID(id string) ([]model.Class, error) {
+func (cs *ClassService) GetClassByCourseID(id string) ([]model.Class, error) {
 	var class []model.Class
-	if err := ps.db.Where("course_id = ?", id).Preload("Course").Find(&class).Error; err != nil {
+	if err := cs.db.Where("course_id = ?", id).Preload("Course").Find(&class).Error; err != nil {
 		return nil, err
 	}
 	return class, nil
 }
 
-func (ps *ClassService) DeleteClassByID(id string) error {
+func (cs *ClassService) DeleteClassByID(id string) error {
 	class := model.Class{}
-	if result := ps.db.Delete(&class, id); result.RowsAffected < 1 {
+	if result := cs.db.Delete(&class, id); result.RowsAffected < 1 {
 		return fmt.Errorf("were not able to delete the class")
 	}
 
