d9f2592659298c344da75c7a37f8d287b9be7a15
Qandle <unmistakework@hotmail.com>
2024-04-15 15:50:44 +0700

    feat[instructor, professor]: basic crud route

--- internal/server/routes/router.go
+++ internal/server/routes/router.go
@@ -14,6 +14,8 @@ func InitializeRoutes(router *gin.Engine, services *service.Service) *gin.Engine
 		FacultyRoutes(api, services.FacultyService)
 		CourseRoutes(api, services.CourseService)
 		ProgramRoutes(api, services.ProgramService)
+		ProfessorRoutes(api, services.ProfessorService)
+		InstructorRoutes(api, services.InstructorService)
 	}
 	return router
 }
