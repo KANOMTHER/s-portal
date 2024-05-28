f542be4f8674fd0b436507c72ca68946641c23e8
Porama6400 <porama@porama.dev>
2024-05-28 16:50:49 +0700

    refactor: remove unused constructors

--- internal/domain/model/student.go
+++ internal/domain/model/student.go
@@ -23,21 +23,3 @@ type Student struct {
 	AdvisorID uint
 	Advisor   Professor
 }
-
-func CreateStudent( programID uint, program Program, degree string, year int, fName string, lName string, dob time.Time, entered time.Time, graduated *time.Time, email string, phone string, advisorID uint, advisor Professor) Student {
-	return Student{
-		ProgramID: programID,
-		Program: program,
-		Degree: degree,
-		Year: year,
-		FName: fName,
-		LName: lName,
-		DOB: dob,
-		Entered: entered,
-		Graduated: graduated,
-		Email: email,
-		Phone: phone,
-		AdvisorID: advisorID,
-		Advisor: advisor,
-	}
-}
